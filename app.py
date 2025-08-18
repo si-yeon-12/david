f9d91533a763ca4624542d451c48127390b923c9
si-yeon-12 <kelly029180@gmail.com>
2025-08-18 13:26:47 +0900

    컴퓨터 이름 표시 기능 추가

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #start
 #branch change
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
 @app.route('/menu')
-def menu_page():
-    return render_template('menu.html')
+def home():
+    if app.debug:
+        hostname='컴퓨터(인스턴스) : '+socket.gethostname()
+    else:
+        hostname=''    
+    
+    return render_template('menu.html', computername=hostname)
 
 if __name__ == '__main__':
     app.run(debug=True)
