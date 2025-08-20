3990eda1a88ec661191e9947ac4aa9b6c3a05e12
si-yeon-12 <kelly029180@gmail.com>
2025-08-20 11:32:05 +0900

    문제 2번 상황 재현

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def home():
     
     return render_template('index.html', computername=hostname)
 
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
