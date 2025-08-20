7e3f8fa475aa862b0c05e5018ce9954fc6c3a674
si-yeon-12 <kelly029180@gmail.com>
2025-08-20 11:28:03 +0900

    test1 코드 추가 in app.py

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def home():
     
     return render_template('index.html', computername=hostname)
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
