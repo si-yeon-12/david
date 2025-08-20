be7231ff499bd745fc4276572a471b31ba98ccd0
si-yeon-12 <kelly029180@gmail.com>
2025-08-20 13:16:49 +0900

    test1 함수 코드를 삭제 - 기획 변경으로 해당 테스트 코드 삭제

--- app.py
+++ app.py
@@ -14,9 +14,5 @@ def home():
     
     return render_template('index.html', computername=hostname)
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
