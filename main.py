05e06afe34a3e9c957cf5dbe4de62a2b7f043750
si-yeon-12 <kelly029180@gmail.com>
2025-08-22 14:30:15 +0900

    main.py 작성 시작

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello Mars")
+def log_read():
+    try:
+        LogFile = open("C:\Codyssey\mission_computer_main.log", 'r') #문자열로 저장됨
+        LogFile = LogFile.read()
+        print(LogFile)
+
+    except FileNotFoundError:
+        print("파일을 찾을 수 없습니다.")
+        return
+    except Exception as e:
+        print("파일 읽는 중 오류 발생 : ", str(e))
+        return
+    
+    LogFile.close()
+    return LogFile
+
+def log_to_list(logfile):
+    LogList = []
+    LogList = logfile.split(",")
